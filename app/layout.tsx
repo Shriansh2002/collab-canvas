794923630dc2b061bc1645012b01167c1079416b
Shriansh Agarwal <41548480+Shriansh2002@users.noreply.github.com>
2024-02-05 15:27:09 +0530

    fix: react suspense for the useSearchParams()

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,8 @@ import { ConvexClientProvider } from "@/providers/convex-client-provider";
 import { Toaster } from "@/components/ui/sonner";
 import { ModalProvider } from "@/providers/modal-provider";
 import { inter } from "@/fonts";
+import { Suspense } from "react";
+import { Loading } from "@/components/auth/loading";
 
 export const metadata: Metadata = {
 	title: "Create Next App",
@@ -18,11 +20,13 @@ export default function RootLayout({
 	return (
 		<html lang='en'>
 			<body className={inter.className}>
-				<ConvexClientProvider>
-					<Toaster />
-					<ModalProvider />
-					{children}
-				</ConvexClientProvider>
+				<Suspense fallback={<Loading />}>
+					<ConvexClientProvider>
+						<Toaster />
+						<ModalProvider />
+						{children}
+					</ConvexClientProvider>
+				</Suspense>
 			</body>
 		</html>
 	);
