8c026af032676db02015d27c5260f2085ecb2ef6
Shriansh Agarwal <41548480+Shriansh2002@users.noreply.github.com>
2024-02-05 17:21:15 +0530

    delete elements

--- app/canvas/[canvasId]/_components/layers/selection-tools.tsx
+++ app/canvas/[canvasId]/_components/layers/selection-tools.tsx
@@ -1,9 +1,15 @@
 "use client";
 import { memo } from "react";
 
+import { Hint } from "@/components/hint";
+import { Button } from "@/components/ui/button";
+import { Trash2 } from "lucide-react";
+
+import { ColorPicker } from "./color-picker";
+
 import { useMutation, useSelf } from "@/liveblocks.config";
 import { useSelectionBounce } from "@/hooks/use-selection-bounce";
-import { ColorPicker } from "./color-picker";
+import { useDeleteLayers } from "@/hooks/use-delete-layers";
 
 import { Color, SelectionToolsProps } from "@/types";
 
@@ -22,6 +28,8 @@ export const SelectionTools = memo(
 			},
 			[selection, setLastUsedColor]
 		);
+
+		const deleteLayers = useDeleteLayers();
 
 		const selectionBounds = useSelectionBounce();
 
@@ -44,6 +52,18 @@ export const SelectionTools = memo(
 				}}
 			>
 				<ColorPicker onChange={setFill} />
+
+				<div className='flex items-center pl-2 ml-2 border-l border-neutral-200'>
+					<Hint label='Delete'>
+						<Button
+							variant='canvasBoard'
+							size='icon'
+							onClick={deleteLayers}
+						>
+							<Trash2 />
+						</Button>
+					</Hint>
+				</div>
 			</div>
 		);
 	}
