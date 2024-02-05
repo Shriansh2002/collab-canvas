be730f3b3be1693bf77a62ad4229b9d895189db0
Shriansh Agarwal <41548480+Shriansh2002@users.noreply.github.com>
2024-02-05 16:35:55 +0530

    Add unselectLayers functionality and update pointer events

--- app/canvas/[canvasId]/_components/main-canvas.tsx
+++ app/canvas/[canvasId]/_components/main-canvas.tsx
@@ -126,6 +126,12 @@ export const Canvas = ({ canvasId }: CanvasProps) => {
 		[canvasState]
 	);
 
+	const unselectLayers = useMutation(({ self, setMyPresence }) => {
+		if (self.presence.selection.length > 0) {
+			setMyPresence({ selection: [] }, { addToHistory: true });
+		}
+	}, []);
+
 	const resizeSelectedLayer = useMutation(
 		({ storage, self }, point: Point) => {
 			if (canvasState.mode !== CanvasMode.Resizing) {
@@ -192,11 +198,34 @@ export const Canvas = ({ canvasId }: CanvasProps) => {
 		setMyPresence({ cursor: null });
 	}, []);
 
+	const onPointerDown = useCallback(
+		(e: React.PointerEvent) => {
+			const point = pointerEventToCanvasPoint(e, camera);
+
+			if (canvasState.mode === CanvasMode.Inserting) {
+				return;
+			}
+
+			setCanvasState({
+				origin: point,
+				mode: CanvasMode.Pressing,
+			});
+		},
+
+		[camera, canvasState.mode, setCanvasState]
+	);
+
 	const onPointerUp = useMutation(
 		({}, e) => {
 			const point = pointerEventToCanvasPoint(e, camera);
 
-			if (canvasState.mode === CanvasMode.Inserting) {
+			if (
+				canvasState.mode === CanvasMode.None ||
+				canvasState.mode === CanvasMode.Pressing
+			) {
+				unselectLayers();
+				setCanvasState({ mode: CanvasMode.None });
+			} else if (canvasState.mode === CanvasMode.Inserting) {
 				insertLayer(canvasState.layerType, point);
 			} else {
 				setCanvasState({ mode: CanvasMode.None });
@@ -204,7 +233,7 @@ export const Canvas = ({ canvasId }: CanvasProps) => {
 
 			history.resume();
 		},
-		[camera, canvasState, history, insertLayer]
+		[camera, canvasState, history, insertLayer, unselectLayers]
 	);
 
 	const selections = useOthersMapped(
@@ -278,6 +307,7 @@ export const Canvas = ({ canvasId }: CanvasProps) => {
 				onPointerMove={onPointerMove}
 				onPointerLeave={onPointerLeave}
 				onPointerUp={onPointerUp}
+				onPointerDown={onPointerDown}
 			>
 				<g
 					style={{
