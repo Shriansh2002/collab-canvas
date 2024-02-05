2d78a73b5cf5c4dc4e0ed5774f87b4c75f9604ac
Shriansh Agarwal <41548480+Shriansh2002@users.noreply.github.com>
2024-02-05 17:30:22 +0530

    feat: Add bring to front and send to back

--- app/canvas/[canvasId]/_components/layers/selection-tools.tsx
+++ app/canvas/[canvasId]/_components/layers/selection-tools.tsx
@@ -3,7 +3,7 @@ import { memo } from "react";
 
 import { Hint } from "@/components/hint";
 import { Button } from "@/components/ui/button";
-import { Trash2 } from "lucide-react";
+import { BringToFront, SendToBack, Trash2 } from "lucide-react";
 
 import { ColorPicker } from "./color-picker";
 
@@ -16,6 +16,49 @@ import { Color, SelectionToolsProps } from "@/types";
 export const SelectionTools = memo(
 	({ camera, setLastUsedColor }: SelectionToolsProps) => {
 		const selection = useSelf((me) => me.presence.selection);
+
+		const moveToBack = useMutation(
+			({ storage }) => {
+				const liveLayerIds = storage.get("layerIds");
+				const indices: number[] = [];
+
+				const arr = liveLayerIds.toImmutable();
+
+				for (let i = 0; i < arr.length; i++) {
+					if (selection.includes(arr[i])) {
+						indices.push(i);
+					}
+				}
+
+				for (let i = 0; i < indices.length; i++) {
+					liveLayerIds.move(indices[i], i);
+				}
+			},
+			[selection]
+		);
+
+		const moveToFront = useMutation(
+			({ storage }) => {
+				const liveLayerIds = storage.get("layerIds");
+				const indices: number[] = [];
+
+				const arr = liveLayerIds.toImmutable();
+
+				for (let i = 0; i < arr.length; i++) {
+					if (selection.includes(arr[i])) {
+						indices.push(i);
+					}
+				}
+
+				for (let i = indices.length - 1; i >= 0; i--) {
+					liveLayerIds.move(
+						indices[i],
+						arr.length - 1 - (indices.length - 1 - i)
+					);
+				}
+			},
+			[selection]
+		);
 
 		const setFill = useMutation(
 			({ storage }, fill: Color) => {
@@ -52,6 +95,27 @@ export const SelectionTools = memo(
 				}}
 			>
 				<ColorPicker onChange={setFill} />
+
+				<div className='flex flex-col gap-y-0.5'>
+					<Hint label='Bring to front'>
+						<Button
+							variant='canvasBoard'
+							size='icon'
+							onClick={moveToFront}
+						>
+							<BringToFront />
+						</Button>
+					</Hint>
+					<Hint label='Send to Back' side='bottom'>
+						<Button
+							variant='canvasBoard'
+							size='icon'
+							onClick={moveToBack}
+						>
+							<SendToBack />
+						</Button>
+					</Hint>
+				</div>
 
 				<div className='flex items-center pl-2 ml-2 border-l border-neutral-200'>
 					<Hint label='Delete'>
