254271be1a177ca531903f1568d63db1106bce3e
Shriansh Agarwal <41548480+Shriansh2002@users.noreply.github.com>
2024-02-06 10:12:25 +0530

    feat: selection

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,5 +1,5 @@
 import { Side } from "@/enums";
-import { Camera, Color } from "@/types";
+import { Camera, Color, Layer } from "@/types";
 import { Point, XYWH } from "@/types/base";
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
@@ -69,4 +69,40 @@ export function resizeBounds(
 	}
 
 	return res;
+}
+
+export function findIntersectingLayersWithRectangle(
+	layerIds: readonly string[],
+	layers: ReadonlyMap<string, Layer>,
+	a: Point,
+	b: Point
+) {
+	const rect = {
+		x: Math.min(a.x, b.x),
+		y: Math.min(a.y, b.y),
+		width: Math.abs(a.x - b.x),
+		height: Math.abs(a.y - b.y),
+	};
+
+	const ids = [];
+
+	for (const layerId of layerIds) {
+		const layer = layers.get(layerId);
+		if (layer == null) {
+			continue;
+		}
+
+		const { x, y, height, width } = layer;
+
+		if (
+			rect.x + rect.width > x &&
+			rect.x < x + width &&
+			rect.y + rect.height > y &&
+			rect.y < y + height
+		) {
+			ids.push(layerId);
+		}
+	}
+
+	return ids;
 }
